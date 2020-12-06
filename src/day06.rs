fbc281af77ecb5af661066b07940cc4be9a56186
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2020-12-06 10:01:42 +0000

    Clenaed up day 06 a little bit

--- src/day06.rs
+++ src/day06.rs
@@ -2,34 +2,32 @@ use std::collections::HashSet;
 
 pub fn day06(input:String){
     //part 1: create sets for each group, then sum their lengths
-    let answers: Vec<HashSet<char>> =
+    let answers =
         input.split("\n\n")
         .map( |g|
             g.chars()
             .filter(|c| *c != '\n')
             .collect::<HashSet<char>>()
-        )
-        .collect();
+        );
 
-    println!("Part 1: {}", answers.iter().fold(0, |acc,s| acc + s.len()));
+    println!("Part 1: {}", answers.fold(0, |acc,s| acc + s.len()));
 
     //part 2: create sets for each individual, get the intersections of each group, then sum their lengths
     //note to self, 'a'..'z' isn't the alphabet, 'a'..='z' is. 
     let alphabet: HashSet<char> = ('a'..='z').collect();
-    let answers2: Vec<HashSet<char>> =
+    let answers2 =
         input.split("\n\n")
         .map( |g|
             g.lines()
             .map( |i|
                 i.chars()
-                .collect::<HashSet<char>>()
+                .collect()
             )
             .fold( alphabet.clone(),
                 |acc,s| acc.intersection(&s).map(|&x|x).collect()
             )
-        )
-        .collect();
+        );
     
-    println!("Part 2: {}", answers2.into_iter().fold(0, |acc,a| acc+a.len()))
+    println!("Part 2: {}", answers2.fold(0, |acc,a| acc+a.len()))
     
 }
