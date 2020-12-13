295cf8761fa6673b73424a855ad15c2270953884
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2020-12-13 15:02:39 +0000

    Refactored part 1 of Day 13 to be Cooler and More Haskell

--- src/day13.rs
+++ src/day13.rs
@@ -12,43 +12,16 @@ pub fn day13(input:String) -> (String,String){
         input_it.next().unwrap()
         .split(",");
 
-    //part 1: find the lowest n st n > start time where n is the arrival of a bus
-    let mut departures: Vec<(i32,i32)> =
-        time_table
-        .clone()
-        .filter(|&v| v != "x")
-        .map( |v|
-            v.parse().expect(":(")
-        )
-        .map( |i|
-            (i,i)
-        )
-        .collect();
+    //part 1: find the lowest time from start from which a bus will arrive
     
-    let under = &|vec:&Vec<(i32,i32)>| 
-        vec.iter()
-        .filter(|&&(_,v)| v < my_time)
-        .count();
-
-    //collect all the minimum values for arrival times greater than start time...
-    while under(&departures) > 0{
-        departures =
-            departures
-            .into_iter()
-            .map( |(i,t)|
-                if t < my_time {(i,i+t)} else {(i,t)}
-            )
-            .collect();
-    }
-
-    //... picking the smallest value
-    let (m_i,m_t) =
-        departures.iter()
-        .fold( (0,i32::MAX), |min, pair|
-            if pair.1 < min.1 {*pair} else {min}
-        );
+    let (m_i, m_t) = 
+        time_table.clone()
+        .filter( |&v| v != "x")
+        .map   ( | v| v.parse::<i32>().unwrap())
+        .map   ( | v| (v, (v - (my_time % v))))
+        .fold  ( (0,i32::MAX), |(i,m),(j,v)| if v < m {(j,v)} else {(i,m)});
     
-    p1 = m_i * (m_t - my_time);
+    p1 = m_i * m_t;
     
     //enumerate the indexes to get the offset from start time
     let enumed_table: Vec<(usize,usize)>= 
@@ -60,14 +33,14 @@ pub fn day13(input:String) -> (String,String){
         )
         .collect();
     
-    //get big modulo for Chinese Remainder theorem
+    //get big modulo M for Chinese Remainder theorem
     let big_modulo: usize = 
         enumed_table.iter()
         .map( |(_,v)| v)
         .product();
 
     //generate values for CRT:
-    //(a,v,m) where x = a mod m, v is an index, x is solution
+    //(a,v,m) where x = a mod m, v is an index, x is solution mod M
     let crt_vals: Vec<(usize,usize,usize)>=
         enumed_table.iter()
         .map( |&(i,v)| (v-(i % v),v,big_modulo/v))
@@ -77,6 +50,7 @@ pub fn day13(input:String) -> (String,String){
 
     for (i,b,m) in crt_vals{
         for n in 0..{
+            //this gets inverse of x in mod m
             if ((m * n) % b) == 1{
                 sum += i * m * n;
                 break
