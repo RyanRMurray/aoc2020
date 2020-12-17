6ac9c9486621b5d9d5d7f9db590e45411b178337
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2020-12-17 09:36:04 +0000

    End of Day 17. ZOOM.

--- src/day17.rs
+++ src/day17.rs
@@ -12,6 +12,7 @@ enum CoOrd{
 type CubeGrid = HashMap<CoOrd,bool>;
 type NextGrid = HashMap<CoOrd,u8>;
 
+//Is this hacky? It's probably fine.
 fn co_ord_add(x: &CoOrd, y: &CoOrd) -> CoOrd{
     match (x,y){
         (CoOrd::Pt3(a,b,c), CoOrd::Pt3(d,e,f)) =>
@@ -23,6 +24,7 @@ fn co_ord_add(x: &CoOrd, y: &CoOrd) -> CoOrd{
     }
 }
 
+//Derive the next grid from the rules and active neighbour counts
 fn resolve_grid(before: &CubeGrid, counts: NextGrid) -> CubeGrid{
     counts.keys()
     .map( |k|
@@ -36,9 +38,10 @@ fn resolve_grid(before: &CubeGrid, counts: NextGrid) -> CubeGrid{
     .collect()
 }
 
+//Apply the rules to the input grid given an adjacency list
 fn cycle(before: &CubeGrid, adjs: &Vec<CoOrd>) -> CubeGrid{
-    let mut next: NextGrid = HashMap::new();
-    let counts: Vec<_> = 
+    let mut counts: NextGrid = HashMap::new();
+    let neighbouring_active: Vec<_> = 
         before.iter()
         .map( |(k,v)|
             if *v {
@@ -50,14 +53,15 @@ fn cycle(before: &CubeGrid, adjs: &Vec<CoOrd>) -> CubeGrid{
         .flatten()
         .collect();
     
-    for pt in counts.into_iter(){
-        let c = next.entry(pt).or_insert(0);
+    for pt in neighbouring_active.into_iter(){
+        let c = counts.entry(pt).or_insert(0);
         *c += 1;
     }
     
-    resolve_grid(before, next)
+    resolve_grid(before, counts)
 }
 
+//Simply counts the active cubes in a grid
 fn count_active(grid: &CubeGrid) -> usize{
     grid.values()
     .filter(|v| **v)
@@ -66,39 +70,60 @@ fn count_active(grid: &CubeGrid) -> usize{
 
 pub fn day17(input:String) -> (String,String){
     let p1: usize;
-    let p2 = 0;
-    let mut adjacencies: Vec<CoOrd> = vec![];
+    let p2: usize;
+    let mut adjacencies_3d: Vec<CoOrd> = vec![];
+    let mut adjacencies_4d: Vec<CoOrd> = vec![];
 
     //very cool nested loop
+    //I could technically just enumerate these by hand but i cannot be bothered.
     for x in -1..=1{
         for y in -1..=1{
             for z in -1..=1{
-                adjacencies.push(CoOrd::Pt3(x,y,z));
+                adjacencies_3d.push(CoOrd::Pt3(x,y,z));
+                //WE NEED TO GO DEEPER
+                for w in -1..=1{
+                    adjacencies_4d.push(CoOrd::Pt4(x,y,z,w));
+                }
             }
         }
     }
-    //delete (0,0,0)
-    adjacencies.remove(13);
+    //delete (0,0,0), (0,0,0,0). Hard coded because nobody can stop me.
+    adjacencies_3d.remove(13);
+    adjacencies_4d.remove(40);
 
-    //parse starter grid
-    let mut grid: CubeGrid = HashMap::new();
+    //parse starter grids in 3d and 4d space
+    let mut grid_3d: CubeGrid = HashMap::new();
+    let mut grid_4d: CubeGrid = HashMap::new();
     let mut y = 0;
     for l in input.lines(){
         let mut x = 0;
         for c in l.chars(){
-            grid.insert(CoOrd::Pt3(x,y,0), c == '#');
+            grid_3d.insert(CoOrd::Pt3(x,y,0), c == '#');
+            grid_4d.insert(CoOrd::Pt4(x,y,0,0), c == '#');
             x += 1;
         }
         y += 1;
     }
 
-    let after_six =
+    //part 1: six cycles in 3d space
+    let after_six_3d =
         (0..6)
-        .fold(grid, |g,_|
-            cycle(&g, &adjacencies)
+        .fold(grid_3d, |g,_|
+            cycle(&g, &adjacencies_3d)
         );
 
-    p1 = count_active(&after_six);
+    p1 = count_active(&after_six_3d);
+
+    //part 2: six cycles in 4d space
+    //re-wrote part 1 solution to use an enum for coordinates, so its just
+    //the same function call with different parameters :]]]
+    let after_six_4d =
+        (0..6)
+        .fold(grid_4d, |g,_|
+            cycle(&g, &adjacencies_4d)
+        );
+
+    p2 = count_active(&after_six_4d);
 
     answer(p1,p2)
 }
