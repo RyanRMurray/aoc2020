8c9f8741717e1fce840ecb42b11c6d9358fa944e
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2020-12-20 11:38:24 +0000

    made part 1 of day 20 much faster

--- src/day20.rs
+++ src/day20.rs
@@ -1,18 +1,18 @@
 use crate::utils::*;
 
+type Trans = (u32,u32);
+type EdgeGroup = Vec<(Vec<bool>,Trans)>;
+
 const RIGHT: [Pt;10] = 
     [(9,1),(9,2),(9,3),(9,4),(9,5),(9,6),(9,7),(9,8),(9,9),(9,10)];
+const TOP: [Pt;10] =
+    [(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1)];
+const LEFT: [Pt;10] = 
+    [(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9),(0,10)];
+const BOT: [Pt;10] =
+    [(0,10),(1,10),(2,10),(3,10),(4,10),(5,10),(6,10),(7,10),(8,10),(9,10)];
 
-//is g_a | g_b a valid configuration?
-fn is_valid_neighbour(g_a:&Grid<bool>, g_b:&Grid<bool>) -> bool{
-
-    RIGHT.iter()
-    .fold(true, |res,p|
-        res &&
-        g_a.at(p) == g_b.at(&p.add((-9,0)))
-    )
-    
-}
+const SIDES: [[Pt;10];4] = [TOP,RIGHT,BOT,LEFT];
 
 fn flip_grid(g:&Grid<bool>) -> Grid<bool>{
     let mut ng: Grid<bool> = Grid::new();
@@ -44,21 +44,34 @@ fn rotate_grid(g:&Grid<bool>) -> Grid<bool>{
     ng
 }
 
-//rotate to see if valid configuration exists
-fn other_can_neighbour(g_a:&Grid<bool>,g_b:&Grid<bool>) -> bool{
-    let trans = [rotate_grid,rotate_grid,rotate_grid,rotate_grid,flip_grid,rotate_grid,rotate_grid,rotate_grid,rotate_grid];
-    let mut a = g_a.clone();
-    let mut b = g_b.clone();
+fn all_possible_edges(g:&Grid<bool>) -> EdgeGroup{
+    //unflipped
+    let mut cp = g.clone();
+    let mut edges: EdgeGroup = vec![];
 
-
-    for a_trans in trans.iter(){
-        for b_trans in trans.iter(){
-            b = b_trans(&b);
-            if is_valid_neighbour(&a,&b){
-                return true
+    for flips in 0..1{
+        for rots in 0..3{
+            for side in SIDES.iter(){
+                edges.push(
+                    (
+                        side.iter().map(|p| *cp.at(p)).collect()
+                    ,
+                        (rots,flips)
+                    )
+                )
             }
+            cp = rotate_grid(&cp)
         }
-        a = a_trans(&a);
+        cp = flip_grid(&cp)
+    }
+    edges
+}
+
+fn valid_neighbours(a:&EdgeGroup,b:&EdgeGroup) -> bool{
+    for (e,_) in a.iter(){
+        if b.iter().map(|(edge,_)| edge.clone()).collect::<Vec<Vec<bool>>>().contains(e){
+            return true
+        }
     }
     false
 }
@@ -84,18 +97,23 @@ pub fn day20(input:String) -> (String,String){
         pieces.push(grid);
     }
 
+    //part 1: find the ids of the corner pieces (by finding pieces with two neighbours)
     let mut neighbours: Vec<i32> = vec![0; pieces.len()];
 
-    for a in 0..pieces.len(){
-        for b in a+1..pieces.len(){
-            if a != b{
-                if other_can_neighbour(&pieces[a], &pieces[b]){
-                    neighbours[a] += 1;
-                    neighbours[b] += 1;
-                }
+    //precalculate all the possible edges a piece can have...
+    let groups: Vec<EdgeGroup> = 
+        pieces.iter()
+        .map(|g| all_possible_edges(&g))
+        .collect();
+
+    //...and record groups that are valid neighbours
+    for a in 0..groups.len(){
+        for b in a+1..groups.len(){
+            if valid_neighbours(&groups[a], &groups[b]){
+                neighbours[a] += 1;
+                neighbours[b] += 1;
             }
         }
-        println!("done {}",a );
     }
 
     p1 =
